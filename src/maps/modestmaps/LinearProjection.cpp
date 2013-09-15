03ee2435392f20519e56937652609ad873ccdb77
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-09-15 14:47:23 -0400

    cleaning

--- src/maps/modestmaps/LinearProjection.cpp
+++
@@ -1,9 +0,0 @@
-#include "LinearProjection.h"
-
-ofPoint LinearProjection::rawProject(ofPoint point) {
-	return ofPoint(point.x, point.y);
-}
-
-ofPoint LinearProjection::rawUnproject(ofPoint point) {
-	return ofPoint(point.x, point.y);
-}
