eed7a4da054cd261247419dcf4d7f2d11071c52e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-09-15 13:10:17 -0400

    adding super particle

---
+++ src/simulators/SuperParticle.h
@@ -0,0 +1,23 @@
+//
+//  SuperParticle.h
+//  ColumbiaLibrary
+//
+//  Created by Patricio Gonzalez Vivo on 9/15/13.
+//
+//
+
+#pragma once
+#include "Particle.h"
+
+class SuperParticle : public Particle {
+public:
+    
+    SuperParticle();
+    
+    void    addNoise(float _angle, float _turbulence);
+    void	applyGravityForceTo(ofPoint *_pos, float _pct);
+    void	applyFlockingForce(ofPoint *_offSet, float _neighbordhood, float _independece);
+    void    applyAttractionToNodes(float _pct);
+    
+    ofPoint localOffset;
+};
